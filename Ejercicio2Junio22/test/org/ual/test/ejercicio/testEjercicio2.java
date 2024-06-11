87a1b8a830ecd51444de70dd8800664d2b99d1df
pms863 <114874858+pms863@users.noreply.github.com>
2024-06-11 18:44:53 +0200

    update

--- Ejercicio2Junio22/test/org/ual/test/ejercicio/testEjercicio2.java
+++ Ejercicio2Junio22/test/org/ual/test/ejercicio/testEjercicio2.java
@@ -3,16 +3,24 @@ package org.ual.test.ejercicio;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 import org.ual.ejercicio.ejercicio2;
 
 public class testEjercicio2 {
 	@ParameterizedTest(name = "todos={0},presentados={1}, el resultado esperado es {2}")
 	@CsvFileSource(resources = "ejercicio.csv", delimiter = ';')
-	public static void testlistaEstudiantesNoPresentados(ArrayList<String> todos, ArrayList<String> presentados,
-			ArrayList<String> esperado) {
+	void testlistaEstudiantesNoPresentados(String todos, String presentados, String esperado) {
 
-		ArrayList<String> result = ejercicio2.listaEstudiantesNoPresentados(new ArrayList<>(todos), new ArrayList<>(presentados));
-		assertEquals(result.toArray(), esperado.toArray());
+		 ArrayList<String> todosList = new ArrayList<>(Arrays.asList(todos.split(",")));
+	        ArrayList<String> presentadosList = new ArrayList<>(Arrays.asList(presentados.split(",")));
+	        ArrayList<String> esperadoList = new ArrayList<>(Arrays.asList(esperado.split(",")));
+	        ejercicio2 ejercicio = new ejercicio2();
+	        ArrayList<String> result = ejercicio.listaEstudiantesNoPresentados(todosList, presentadosList);
+		assertEquals(result, esperadoList);
 	}
 }
